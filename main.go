ef847d67ae9c1d9cf832773ef4c33d546e5ce11a
Yuyq1114 <bk642286@gmail.com>
2022-09-03 22:30:04 +0800

    commit1

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func printtest() {
+	fmt.Println("print func commit")
+}
+
+func printtest1() {
+	fmt.Println("print func commit1")
+}
+
+func main() {
+	fmt.Println("git_test")
+	fmt.Println("this is vision 2")
+	fmt.Println("this is master")
+}
