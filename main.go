acaef2907e407ba7e8aef49b93420e5ee3b5f087
Yuyq1114 <bk642286@gmail.com>
2022-09-03 22:45:15 +0800

    branch2

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ func printtest() {
 func printtest1() {
 	fmt.Println("print func commit1")
 }
-
+func gitbranch1() {
+	fmt.Println("branch1")
+}
 func main() {
 	fmt.Println("git_test")
 	fmt.Println("this is vision 2")
